d63df556f82e77267a167af971ed71780e963033
Yaroslav Fedoseev <yaroslav.site@mail.ru>
2026-08-22 18:38:26 +0700

    Initial commit v2

--- src/main.ts
+++ src/main.ts
@@ -5,4 +5,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+
+bootstrap().catch((error: unknown) => {
+  console.error('Failed to start application', error);
+  process.exit(1);
+});
